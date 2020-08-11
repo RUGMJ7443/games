c817a96bb3c55559499cb2258eae9c5621b0b0a2
RUGMJ7443 <matthewrhysjones@icloud.com>
2020-08-11 14:33:43 +0100

    website redo

---
+++ script.js
@@ -0,0 +1,8 @@
+function switchGame(game) {
+    var ul = document.getElementById("gamesList");
+  var li = document.createElement("li");
+  li.appendChild(document.createTextNode("Loading..."));
+  ul.appendChild(li);
+  li.classList.add("info");
+    location.href = game;
+}
